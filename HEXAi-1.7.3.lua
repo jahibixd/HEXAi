b76b5d3c822c240152fe8e491842fbda563029bf
jahibixd <134812947+jahibixd@users.noreply.github.com>
2023-05-28 01:38:10 +0300

    Update HEXAi-1.7.3.lua

--- HEXAi-1.7.3.lua
+++ HEXAi-1.7.3.lua
@@ -177,4 +177,4 @@ end
 
 gameFreeze(2)
 
-game.Players.LocalPlayer:Kick("You are not whiteliste. discord.gg/rDCCnFdbb4")
+game.Players.LocalPlayer:Kick("You are not whitelisted. discord.gg/rDCCnFdbb4")
