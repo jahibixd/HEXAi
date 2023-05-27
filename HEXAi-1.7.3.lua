8d124b8aaede8da29ea2a96bbeff440f4b1da5ad
jahibixd <134812947+jahibixd@users.noreply.github.com>
2023-05-28 01:02:18 +0300

    Update HEXAi-1.7.3.lua

--- HEXAi-1.7.3.lua
+++ HEXAi-1.7.3.lua
@@ -1,30 +1,7 @@
--- if loaded(true) do (remove("--")
--- if loaded(false) do
---local ServerSide == ("v1.7.3")
---game.player(status(randomseed) == server(find("ServerSideEvent")
---local function ServerSide(player)
-    print(player.Name.."initiated HEXAi Lauch") after
-    print("loading.")
-    print("loading..")
-    print("loading...")
-
--- getupvalue(Paused("3")
--- sethook(ServerSideRemote) == true
---end
---game.ReplicatedStorage.Server.OnServerEvent:Connect(function(player)
---    ServerSide(player)
---    end)
-
--- loaders(master(https://loadstring/loaders) == true to (localService)
-
-print("Finished Loading in 3 ms")
-
---game.ServerSide(Allow)
--- getlocal(LoadingEvent.hexai) == true then
--- resume("1.7.3") == CurrentVersion
--- source(hexai.ServerSideRemote)(function.Server)
--- if loaded(true) then
-
+print("initiated HEXAi Lauch")
+print("loading.")
+print("loading..")
+print("loading...")
+print("Finished Loading in 1 ms")
 print("Welcome to, HEXAi v1.7.3")
-
 game.Players.LocalPlayer:Kick("Byfron Detected, Error Loading discord.gg/rDCCnFdbb4")
