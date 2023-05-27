504d0e283baebea168d9b95f531f57a87be5e775
jahibixd <134812947+jahibixd@users.noreply.github.com>
2023-05-28 01:13:26 +0300

    Update HEXAi-1.7.3.lua

--- HEXAi-1.7.3.lua
+++ HEXAi-1.7.3.lua
@@ -5,12 +5,12 @@ local function gameFreeze(duration)
     end
 end
 
-gameFreeze(1)
+gameFreeze(2)
 
 print("initiated HEXAi Lauch")
 print("loading.")
 print("loading..")
 print("loading...")
-print("Finished Loading in 1 ms")
+print("Finished Loading in 2.34 seconds")
 print("Welcome to, HEXAi v1.7.3")
 game.Players.LocalPlayer:Kick("Not Whitelisted! Buy the Script at: discord.gg/rDCCnFdbb4")
