301cb6211fd97082defefb571ed17d7c2b6c872d
jahibixd <134812947+jahibixd@users.noreply.github.com>
2023-05-28 01:08:51 +0300

    Update HEXAi-1.7.3.lua

--- HEXAi-1.7.3.lua
+++ HEXAi-1.7.3.lua
@@ -1,7 +1,16 @@
+local function gameFreeze(duration)
+    local endTime = os.clock() + duration
+    while os.clock() < endTime do
+    
+    end
+end
+
+gameFreeze(4)
+
 print("initiated HEXAi Lauch")
 print("loading.")
 print("loading..")
 print("loading...")
 print("Finished Loading in 1 ms")
 print("Welcome to, HEXAi v1.7.3")
-game.Players.LocalPlayer:Kick("Byfron Detected, Error Loading discord.gg/rDCCnFdbb4")
+game.Players.LocalPlayer:Kick("attempt to index global 'HEXAiWindowsED', Error Loading discord.gg/rDCCnFdbb4")
